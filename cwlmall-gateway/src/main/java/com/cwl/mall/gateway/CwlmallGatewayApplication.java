e738549d13d297373225db2a6d04991c39ee93e6
cwl <1045501964@qq.com>
2022-03-17 09:18:39 +0800

    添加GateWay网关微服务，修改端口

---
+++ cwlmall-gateway/src/main/java/com/cwl/mall/gateway/CwlmallGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.cwl.mall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CwlmallGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CwlmallGatewayApplication.class,args);
+    }
+}
