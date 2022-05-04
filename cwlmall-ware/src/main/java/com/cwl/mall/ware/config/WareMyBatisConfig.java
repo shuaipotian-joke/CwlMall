c90de2553503639bec484ec5ebb397ea45f1625d
cwl <1045501964@qq.com>
2022-05-04 21:13:20 +0800

    修复包路径异常bug

--- cwlmall-ware/src/main/java/com/cwl/mall/ware/config/WareMyBatisConfig.java
+++ cwlmall-ware/src/main/java/com/cwl/mall/ware/config/WareMyBatisConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
-@MapperScan("com.atguigu.gulimall.ware.dao")
+@MapperScan("com.cwl.mall.ware.dao")
 @Configuration
 public class WareMyBatisConfig {
 
